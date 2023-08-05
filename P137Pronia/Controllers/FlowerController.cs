ad8923c91a742a83f4a3e5b361f06f716ec62fe5
Adil Tehranli <adiljt@code.edu.az>
2023-08-05 18:27:57 +0400

    Updated Flower index

--- P137Pronia/Controllers/FlowerController.cs
+++ P137Pronia/Controllers/FlowerController.cs
@@ -1,21 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using P137Pronia.DataAccess;
+using P137Pronia.Models;
 using P137Pronia.Services.Interfaces;
+using P137Pronia.ViewModels.ProductVMs;
 
 namespace P137Pronia.Controllers;
 
 public class FlowerController : Controller
 {
     readonly IProductService _service;
+    readonly ProniaDbContext _context;
 
-    public FlowerController(IProductService service)
+    public FlowerController(IProductService service, ProniaDbContext context)
     {
         _service = service;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult>  Index()
     {
-        return View();
+        IQueryable<Product> products = _service.GetTable;
+        GetShopVM shopVM = new GetShopVM
+        {
+            Products= products.Include(p => p.ProductImages),
+            ProductCount=await products.CountAsync(),
+            Categories=_context.Categories.Include(p => p.ProductCategories),
+            CategoryCount=await _context.ProductCategories.CountAsync(),
+        };
+        return View(shopVM);
     }
     public async Task<IActionResult> Detail(int? id)
     {

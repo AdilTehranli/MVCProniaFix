82b9c25836484e18ace86ff2a18697a515a788d9
Adil Tehranli <adiljt@code.edu.az>
2023-08-09 20:13:24 +0400

    Update auth

--- P137Pronia/Models/ProductComment.cs
+++ P137Pronia/Models/ProductComment.cs
@@ -1,6 +1,13 @@
 ﻿namespace P137Pronia.Models;
 
-public class ProductComment
+public class ProductComment:BaseEntity
 {
+    public string AppUserId { get; set; }
+    public int ProductId { get; set; }
+    public DateTime PostedTime { get; set; }
+    public AppUser AppUser { get; set; }
+    public Product Product { get; set; }
+    public  bool IsDeleted { get; set; }
+
 
 }
